69c8ed038f8c723e7c7684ebb703b42725a73dec
viktorshopa <124473891+starbowteam@users.noreply.github.com>
2026-05-18 00:24:15 +0800

    Update diamond-ui.js

--- js/diamond-ui.js
+++ js/diamond-ui.js
@@ -563,7 +563,7 @@ function showDisclaimerModal() {
     overlay.className = 'disclaimer-modal-overlay';
     overlay.innerHTML = `
         <div class="disclaimer-modal">
-            <div class="disclaimer-modal-icon"><img src="assets/logo.png" style="width: 70px; height: 70px; border-radius: 12px;"></div>
+            <div class="disclaimer-modal-icon"><i class="fas fa-book "></i></div>
             <h3>${t('disclaimerTitle')}</h3>
             <p>${t('disclaimerText')}</p>
             <button>${t('disclaimerClose')}</button>
